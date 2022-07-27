dd5935b634cff6aca947e6e8aedbcdafd7ab8384
Jarif Hosain <hosainjarif@gmail.com>
2022-07-27 13:13:58 -0400

    Done to code this game

--- 2048/app.js
+++ 2048/app.js
@@ -9,34 +9,140 @@ grid.randomEmptyCell().tile = new Tile(gameBoard)
 setupInput();
 
 
-
-
 function setupInput() {
     window.addEventListener("keydown", handleInput, { once: true })
 }
 
-function handleInput(e) {
+async function handleInput(e) {
     console.log(e.key);
     switch (e.key) {
         case "ArrowUp":
-            moveUp()
+            if (!canMoveUp()) {
+                setupInput();
+                return;
+            }
+            await moveUp();
             break;
         case "ArrowDown":
-            moveDown()
+            if (!canMoveDown()) {
+                setupInput();
+                return;
+            }
+            await moveDown();
             break;
         case "ArrowRight":
-            moveRight()
+            if (!canMoveRight()) {
+                setupInput();
+                return;
+            }
+            await moveRight();
             break;
         case "ArrowLeft":
-            moveLeft()
+            if (!canMoveLeft()) {
+                setupInput();
+                return;
+            }
+            await moveLeft();
             break;
         default:
-            setupInput()
-            return 
+            setupInput();
+            return
     };
 
+    grid.cells.forEach(cell => cell.mergeTiles())
+
+
+    const newTile = new Tile(gameBoard);
+    grid.randomEmptyCell().tile = newTile;
+
+
+    if (!canMoveUp && !canMoveDown && !canMoveLeft && !canMoveRight) {
+        newTile.waitForTransition(true).then(() => {
+            alert(`You lose the game!
+                       Try again`);
+        });
+        return
+    }
     setupInput();
 };
-function moveUp(){
-    slideTiles(grid.cellsByColumn)
+
+
+
+function moveUp() {
+    return slideTiles(grid.cellsByColumn)
+}
+function moveRight() {
+    return slideTiles(grid.cellsByRow.map(row => [...row].reverse()));
+}
+function moveDown() {
+    return slideTiles(grid.cellsByColumn.map(row => [...row].reverse()));
+}
+function moveLeft() {
+    return slideTiles(grid.cellsByRow)
+}
+
+function slideTiles(cells) {
+
+    return Promise.all(
+
+        cells.flatMap(group => {
+
+            const promise = [];
+            for (let i = 1; i < group.length; i++) {
+                const cell = group[i];
+
+                if (cell.tile == null) continue;
+
+                let lastValidCell;
+
+
+                for (let j = i - 1; j >= 0; j--) {
+                    const moveToCell = group[j];
+                    if (!moveToCell.canAccept(cell.tile)) break;
+                    lastValidCell = moveToCell;
+                };
+
+
+                if (lastValidCell != null) {
+
+                    promise.push(cell.tile.waitForTransition())
+                    if (lastValidCell.tile != null) {
+                        lastValidCell.mergeTile = cell.tile;
+                    } else {
+                        lastValidCell.tile = cell.tile;
+                    }
+                    cell.tile = null;
+                };
+
+            };
+
+            return promise;
+        })
+    );
+}
+
+
+function canMoveUp() {
+    return canMove(grid.cellsByColumn);
+};
+function canMoveDown() {
+    return canMove(grid.cellsByColumn.map(row => [...row].reverse()));
+};
+function canMoveRight() {
+    return canMove(grid.cellsByRow.map(row => [...row].reverse()));
+};
+function canMoveLeft() {
+    return canMove(grid.cellsByRow);
+};
+
+
+function canMove(cells) {
+    return cells.some(group => {
+        return group.some((cell, index) => {
+            if (index === 0) return false
+            if (cell.tile == null) return false
+            const moveToCell = group[index - 1]
+            return moveToCell.canAccept(cell.tile)
+        })
+    })
 }
